81fd177c143fcf787fb0bf21b9dbc08dd75697a5
kk99351 <kk99351@gmail.com>
2023-04-06 13:25:41 +0530

    react-meetup

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+export default function NewMeetupPage() {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup = {addMeetupHandler} />
+}
